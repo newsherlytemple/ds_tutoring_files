eb66dc4e8b89c65ec1500a402430039398d12df2
newsherlytemple <sherlyyaghoubi@gmail.com>
2025-02-25 18:19:41 -0800

    example for bag BagInterface

---
+++ BagExample/BagInterface.java
@@ -0,0 +1,28 @@
+package BagExample;
+
+public interface BagInterface<T> {
+
+    // a bag is a abstract data type 
+    // way to think about this: its some data structure that we can implement!
+    // there's not one standard way to implement, so Java doesn't have a built-in bag class
+   
+    // a bag is some collection. any collection
+    // has items in no particular order
+    // can have duplicates
+    // can have any number of items
+    // can be empty
+    // can add and remove items as we go thru the bag w/ an iterator
+    // don't need to know indices b/c there's no inherent order
+
+    // methods for our bag, from your notes (ch01)
+    public int getCurrentSize();
+    public boolean isEmpty();
+    public boolean add(T newEntry);
+    public T remove();
+    public boolean remove(T anEntry);
+    public void clear();
+    public int getFrequencyOf(T anEntry);
+    public boolean contains(T anEntry);
+    public T[] toArray();
+
+} 
